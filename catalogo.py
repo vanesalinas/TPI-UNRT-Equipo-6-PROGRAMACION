c9846c2ebb44d26986a8677b5680f46b92a98482
toto4723 <toto47perez@gmail.com>
2026-06-17 20:20:35 -0300

    catalogo

---
+++ catalogo.py
@@ -0,0 +1,67 @@
+catalogo=[
+        {
+        "Nombre cafe":"Kenia SL28 — Embu",
+        "Tostaderia": "Café Cumbal (Mendoza)",
+        "Origen": "Embu, Kenia",
+        "Proceso": "Lavado",
+        "SCA":"85",
+        "Notas de sabor": "Te negro, pomelo, vainilla"
+    },
+        {
+        "Nombre cafe":"Colombia Caturra — Honey",
+        "Tostaderia": "Café Cumbal (Mendoza)",
+        "Origen": "Huila, Colombia",
+        "Proceso": "Honey",
+        "SCA":"84.75",
+        "Notas de sabor": "Chocolate, caramelo, cítrico, miel"
+    },
+        {
+        "Nombre cafe":"Etiopía Heirloom — Oromia",
+        "Tostaderia": "Café Cumbal (Mendoza)",
+        "Origen": "Oromia, Etiopía",
+        "Proceso": "Lavado",
+        "SCA":"84",
+        "Notas de sabor": "Cacao, pasas, especias rojas"
+    },
+        {
+        "Nombre cafe":"El Salvador Montealegre",
+        "Tostaderia": "John & Joe (Buenos Aires)",
+        "Origen": "Volcán Tecapa, El Salvador",
+        "Proceso": "Natural",
+        "SCA":"85",
+        "Notas de sabor": "Piña, uva roja, dátiles, dulzor marcado"
+    },
+        {
+        "Nombre cafe":"Colombia Supremo",
+        "Tostaderia": "John & Joe (Buenos Aires)",
+        "Origen": "Nariño, Huila, Antioquia",
+        "Proceso": "Lavado",
+        "SCA":"85",
+        "Notas de sabor": "Almendras tostadas, chocolate, suave"
+    },
+        {
+        "Nombre cafe":"Brazil Santos Cerrado",
+        "Tostaderia": "John & Joe (Buenos Aires)",
+        "Origen": "Cerrado Mineiro, Brasil",
+        "Proceso": "Natural",
+        "SCA":"84",
+        "Notas de sabor": "Frutos secos, chocolate, gran cuerpo"
+    },
+        {
+        "Nombre cafe":"Bolivia Yanaloma",
+        "Tostaderia": "John & Joe (Buenos Aires)",
+        "Origen": "Apolo, La Paz",
+        "Proceso": "Lavado",
+        "SCA":"84.5",
+        "Notas de sabor": "Chocolate suave, frutos secos, manjar"
+    },
+        {
+        "Nombre cafe":"Brazil Amarelo Santos",
+        "Tostaderia": "John & Joe (Buenos Aires)",
+        "Origen": "Brejetuba, Espíritu Santo",
+        "Proceso": "Lavado",
+        "SCA":"85",
+        "Notas de sabor": "Frutilla, mora, azúcar mascabo"
+    }
+]
+
---
+++ main.py
@@ -0,0 +1,3 @@
+from catalogo import catalogo
+
+
