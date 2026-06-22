93c63369a3cb89acb9f6f583a74047dfc8123775
prisci567 <priscilavergara310@gmail.com>
2026-06-22 14:26:21 -0300

    Update main.py

--- main.py
+++ main.py
@@ -14,65 +14,21 @@ def preguntas ():
     
     return respuestas
     
-def recomendador(respuestas):
-    leche = respuestas[0]
-    notas = respuestas[1]
-    intensidad = respuestas[2]
+cafe, coincidencias = recomendador(respuestas)
 
-    mejor_cafe      = None
-    mejor_puntaje   = 0          
-
-    for cafe in catalogo:
-        coincide_leche = cafe["Con leche"] == leche or cafe["Con leche"] == "ambos"
-        coincide_notas = notas in cafe["Notas de sabor"]
-        coincide_intesidad = cafe["Intensidad"] == intensidad
-
-        puntaje = sum([coincide_leche, coincide_notas, coincide_intesidad]) #cuenta cuantos son TRRUE
-
-        if puntaje == 3:
-            return cafe, [leche, notas, intensidad]
-        
-        if puntaje > mejor_puntaje:
-            mejor_puntaje = puntaje
-            mejor_cafe = cafe
-            mejor_coincidencias = []
-            if coincide_leche:
-                mejor_coincidencias.append(leche)
-            if coincide_notas:
-                mejor_coincidencias.append(notas)
-            if coincide_intesidad:
-                mejor_coincidencias.append(intensidad)
-
-    if mejor_cafe:
-        return mejor_cafe, mejor_coincidencias
-
-respuestas = preguntas()
-cafes_recomendados, coincidencias = recomendador(respuestas)
-
-if cafes_recomendados:
-
+if cafe:
     if len(coincidencias) == 3:
-        cafe = cafes_recomendados[0]
-
-        print(f"\n☕ Tu café ideal: {cafe['Nombre cafe']}")
+        print(f"☕ Tu café ideal: {cafe['Nombre cafe']}")
         print(f"Tostadería : {cafe['Tostaderia']}")
         print(f"Origen     : {cafe['Origen']}")
         print(f"Proceso    : {cafe['Proceso']}")
         print(f"Intensidad : {cafe['Intensidad']}")
         print(f"Notas      : {cafe['Notas de sabor']}")
-
     else:
-        print("\nNo encontramos una coincidencia exacta.")
-        print("☕ Te recomendamos estas opciones:\n")
-
-        for cafe in cafes_recomendados[:3]:  # muestra hasta 3 cafés
-            print(f"➡ {cafe['Nombre cafe']}")
-            print(f"   Tostadería: {cafe['Tostaderia']}")
-            print(f"   Origen: {cafe['Origen']}")
-            print(f"   Notas: {cafe['Notas de sabor']}")
-            print()
-
-        print(f"Coincidencias encontradas: {', '.join(coincidencias)}")
-
+        print(f"No encontramos coincidencia exacta.")
+        print(f"☕ Pero te recomendamos probar: {cafe['Nombre cafe']}")
+        print(f"   Notas: {cafe['Notas de sabor']}")
+        print(f"Coincide con tu preferencia de: {', '.join(coincidencias)}")
 else:
     print("No encontramos ningún café para tu perfil.")
+
