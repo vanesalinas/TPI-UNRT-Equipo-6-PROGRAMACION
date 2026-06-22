aad84c7c1b62dd5750153cee3499e6c432aa2efb
prisci567 <priscilavergara310@gmail.com>
2026-06-21 21:42:37 -0300

    le añadi recomendaciones

--- main.py
+++ main.py
@@ -27,3 +27,34 @@ def recomendador(respuestas):
                 mejor_coincidencias.append(notas)
             if coincide_intesidad:
                 mejor_coincidencias.append(intensidad)
+              
+                respuestas = preguntas()
+cafes_recomendados, coincidencias = recomendador(respuestas)
+
+if cafes_recomendados:
+
+    if len(coincidencias) == 3:
+        cafe = cafes_recomendados[0]
+
+        print(f"\n☕ Tu café ideal: {cafe['Nombre cafe']}")
+        print(f"Tostadería : {cafe['Tostaderia']}")
+        print(f"Origen     : {cafe['Origen']}")
+        print(f"Proceso    : {cafe['Proceso']}")
+        print(f"Intensidad : {cafe['Intensidad']}")
+        print(f"Notas      : {cafe['Notas de sabor']}")
+
+    else:
+        print("\nNo encontramos una coincidencia exacta.")
+        print("☕ Te recomendamos estas opciones:\n")
+
+        for cafe in cafes_recomendados[:3]:  # muestra hasta 3 cafés
+            print(f"➡ {cafe['Nombre cafe']}")
+            print(f"   Tostadería: {cafe['Tostaderia']}")
+            print(f"   Origen: {cafe['Origen']}")
+            print(f"   Notas: {cafe['Notas de sabor']}")
+            print()
+
+        print(f"Coincidencias encontradas: {', '.join(coincidencias)}")
+
+else:
+    print("No encontramos ningún café para tu perfil.")
